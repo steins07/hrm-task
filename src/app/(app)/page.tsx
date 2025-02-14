90c6a2f83dfbc9e49582b2dfd19b571cfd959328
Romus Shrestha <sromus498@gmail.com>
2025-02-14 14:23:22 +0545

    added credentails detail for login

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -18,6 +18,15 @@ export default function Home() {
             Login To Get Started
           </li>
           <li>And try out the features that were listed in the task</li>
+          <li>use following credentials to test:
+            <ul className="pl-6"><li>You can use either username or email</li></ul>
+            <ul className="pl-6">
+              <li>Username: Romus Shrestha</li>
+              <li>Email: test@gmail.com</li>
+              <li>Password: test1234</li>
+            </ul>
+          </li>
+          <li>For other user credentials is respective username and password are same for all user</li>
         </ol>
       </main>
 
