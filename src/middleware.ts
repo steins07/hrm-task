12e04e454aadd504d3163357f455dc69645695b3
Romus Shrestha <sromus498@gmail.com>
2025-02-14 13:51:41 +0545

    updated middleware for auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,34 @@
+
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 export { default } from 'next-auth/middleware';
 
 export const config = {
-    matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/employee/:path*'],
+    matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/employee/:path*','/dummy-page'],
 };
 
 export async function middleware(request: NextRequest) {
     const token = await getToken({ req: request });
-    console.log(token?.role);
-    const role = token?.role
-    console.log("Roles", role);
-
+    const role = token?.role;
     const url = request.nextUrl;
-    // Redirect to dashboard if the user is already authenticated
-    // and trying to access sign-in, sign-up, or home page
-    console.log("token", token?.role)
-    if (
-        token && role === 'HR' &&
-        (
-            url.pathname.startsWith('/sign-in') ||
-            url.pathname.startsWith('/sign-up') ||
-            url.pathname === '/')
-    ) {
+
+
+    // Redirect HR users to dashboard if they try to access sign-in, sign-up, or home 
+    if (token && role === 'HR' && (
+        url.pathname.startsWith('/sign-in') ||
+        url.pathname.startsWith('/sign-up') ||
+        url.pathname === '/')) {
         return NextResponse.redirect(new URL('/dashboard', request.url));
     }
-   
 
-    if (!token && (
-        url.pathname.startsWith('/dashboard') ||
-        url.pathname.startsWith('/employee')
-    )) {
+    // Redirect non-authenticated users trying to access protected routes
+    if (!token && (url.pathname.startsWith('/dashboard') || url.pathname.startsWith('/employee'))) {
         return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
+
+    // Redirect users who are NOT HR when trying to access restricted areas
+    if (token && role !== 'HR' && url.pathname.startsWith('/employee')) {
+        return NextResponse.redirect(new URL('/dummy-page', request.url));
     }
 
     return NextResponse.next();
