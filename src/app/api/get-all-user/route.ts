b187c4ba506d3774ac45dd617b7b3390ed73b945
Romus Shrestha <sromus498@gmail.com>
2025-02-14 13:55:59 +0545

    cleaned code

--- src/app/api/get-all-user/route.ts
+++ src/app/api/get-all-user/route.ts
@@ -1,26 +1,11 @@
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "../auth/[...nextauth]/options";
+
 import dbConnect from "@/lib/dbConnect";
 import {UserModel} from "@/model/AllModels";
-// import { User } from "next-auth";
-// import mongoose from "mongoose";
+
 
 
 export async function GET() {
   await dbConnect();
-
-//   const session = await getServerSession(authOptions);
-//   console.log("session", session);
-//   //check session and user available
-//   if (!session || !session.user) {
-//     return Response.json(
-//       {
-//         success: false,
-//         message: "Not Authenticated",
-//       },
-//       { status: 401 }
-//     );
-//   }
 
   try {
     const users = await UserModel.find().populate('managerId employees attendance leaveRequests')
