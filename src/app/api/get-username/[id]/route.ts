6545b488e69b8ddfbb9977d006be0086f686d0b7
Romus Shrestha <sromus498@gmail.com>
2025-02-17 16:27:45 +0545

    add route to get username

---
+++ src/app/api/get-username/[id]/route.ts
@@ -0,0 +1,42 @@
+import dbConnect from "@/lib/dbConnect";
+import { UserModel } from "@/model/AllModels";
+import { NextRequest } from "next/server";
+
+type Params = { id: string };
+
+export async function GET(request: NextRequest, { params }: { params: Params }) {
+    await dbConnect();
+
+    try {
+        const { id } = params;
+
+        console.log("ID:",id);
+        if (!id || typeof id !== "string") {
+            return Response.json(
+                { success: false, messages: "Invalid or missing user ID", data:null },
+                { status: 400 }
+            );
+        }
+
+        const user = await UserModel.findById(id).select("username");
+        console.log("User:",user);
+
+        if (!user) {
+            return Response.json(
+                { success: false, messages: "User not found", data: null },
+                { status: 404 }
+            );
+        }
+
+        return Response.json(
+            { success: true, messages: "User found", data: user.username },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        return Response.json(
+            { success: false, messages: "Internal Server Error", data: null },
+            { status: 500 }
+        );
+    }
+}
