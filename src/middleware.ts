6fd619704827d436acc7aacc7bef540ec9dcffad
Romus Shrestha <sromus498@gmail.com>
2025-02-17 17:12:52 +0545

    access change to urls

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { getToken } from 'next-auth/jwt';
 export { default } from 'next-auth/middleware';
 
 export const config = {
-    matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/employee/:path*','/dummy-page','/add-employee'],
+    matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/employee/:path*', '/dummy-page', '/add-employee'],
 };
 
 export async function middleware(request: NextRequest) {
@@ -22,17 +22,17 @@ export async function middleware(request: NextRequest) {
     }
 
     // Redirect non-authenticated users trying to access protected routes
-    if (!token && (url.pathname.startsWith('/dashboard') || url.pathname.startsWith('/employee')||url.pathname === '/add-employee')) {
+    if (!token && (url.pathname.startsWith('/dashboard') || url.pathname.startsWith('/employee') || url.pathname.startsWith('/add-employee'))) {
         return NextResponse.redirect(new URL('/sign-in', request.url));
     }
 
     // Redirect users who are NOT HR when trying to access restricted areas
-    if (token && role !== 'HR' && (url.pathname.startsWith('/employee')||url.pathname === '/add-employee')||url.pathname === '/') {
+    if (token && role !== 'HR' && (url.pathname.startsWith('/employee') || url.pathname === '/add-employee')) {
         return NextResponse.redirect(new URL('/dummy-page', request.url));
     }
 
     // Redirect users who are NOT HR when trying to access restricted areas
-    if (token && role !== 'HR' && (url.pathname.startsWith('/sign-up')||url.pathname === '/sign-in'||url.pathname === '/')) {
+    if (token && role !== 'HR' && (url.pathname.startsWith('/sign-up') || url.pathname.startsWith('/sign-in') || url.pathname === '/')) {
         return NextResponse.redirect(new URL('/dashboard', request.url));
     }
 
